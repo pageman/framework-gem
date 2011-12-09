79b45168b06e004cb07282511ed5975a9b4c1005
Jarrod Taylor <jarrodtaylor@me.com>
2011-12-09 18:42:10 -0500

    Adds optional mysql support to framework

--- lib/vesper/template/config/databases.rb
+++ lib/vesper/template/config/databases.rb
@@ -2,3 +2,12 @@
 # DataMapper::Logger.new($stdout, :debug)
 
 DataMapper.setup(:default, "sqlite://#{Dir.pwd}/data/development.sqlite3")
+
+# configure :production do
+#   DataMapper.setup(:default, {
+#     :adapter  => 'mysql',
+#     :host     => '',
+#     :username => '' ,
+#     :password => '',
+#     :database => ''})
+# end
