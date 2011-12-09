d2a7d019989683d8098e212b790ded79633e1631
Jarrod Taylor <jarrodtaylor@me.com>
2011-12-09 15:12:29 -0500

    Adds to_fields method to dates and times in framework

--- lib/vesper/template/ext/date_time.rb
+++ lib/vesper/template/ext/date_time.rb
@@ -16,6 +16,30 @@ module Timepiece
     self.strftime string
   end
 
+  def to_fields field
+    date_field = ""
+    
+    date_field << "<select name='#{field}_month' id='#{field}_month'>"
+    (1..12).each do |m|
+      date_field << "<option value='#{m}' #{'selected' if m == self.strftime('%m').to_i}>#{m}</option>"
+    end
+    date_field << "</select>"
+    
+    date_field << "<select name='#{field}_day' id='#{field}_day'>"
+    (1..31).each do |d|
+      date_field << "<option value='#{d}' #{'selected' if d == self.strftime('%d').to_i}>#{d}</option>"
+    end
+    date_field << "</select>"
+    
+    date_field << "<select name='#{field}_year' id='#{field}_year'>"
+    (2007..Chronic.parse('3 years from now').strftime('%Y').to_i).each do |y|
+      date_field << "<option value='#{y}' #{'selected' if y == self.strftime('%Y').to_i}>#{y}</option>"
+    end
+    date_field << "</select>"
+    
+    date_field
+  end
+
 end
 
 DateTime.class_eval do
