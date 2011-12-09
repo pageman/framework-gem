91e8641cf787cd5edf3bd93d857a4af851b69deb
Jarrod Taylor <jarrodtaylor@me.com>
2011-12-09 16:21:48 -0500

    Moves truncate_to_decimal framework extension into Number module

--- lib/vesper/template/ext/float.rb
+++
@@ -1,9 +0,0 @@
-Float.class_eval do
-  
-  def truncate_to_decimal dec = 1
-    rounded = "%.#{dec}f" % self
-    rounded = rounded.to_s.split('.').first if rounded.to_s.end_with?('0')
-    rounded.to_f
-  end
-  
-end
--- lib/vesper/template/ext/number.rb
+++ lib/vesper/template/ext/number.rb
@@ -13,6 +13,13 @@ module Number
     self%2 != 0
   end
 
+  def truncate_to_decimal dec = 1
+    num = self
+    rounded = "%.#{dec}f" % num.to_f
+    rounded = rounded.to_s.split('.').first if rounded.to_s.end_with?('0')
+    rounded.to_f
+  end
+
 end
 
 Fixnum.class_eval do
