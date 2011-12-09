e531f3babefed44a217951631bd61cf7aec513be
Jarrod Taylor <jarrodtaylor@me.com>
2011-12-09 01:15:19 -0500

    Adds String.truncate framework extension

--- lib/vesper/template/ext/string.rb
+++ lib/vesper/template/ext/string.rb
@@ -9,5 +9,12 @@ String.class_eval do
     self.split(' ').each {|s| title << s.capitalize + ' ' }
     title
   end
-  
+
+  # Truncates by word count, not letter
+  def truncate(length = 100, end_string = '...')
+    return if self == nil
+    words = self.split()
+    words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+  end
+
 end
