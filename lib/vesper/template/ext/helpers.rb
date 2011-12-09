eb2a1adff47d09f6764c745e60ce5eeac7ad313c
Jarrod Taylor <jarrodtaylor@me.com>
2011-12-09 11:07:43 -0500

    Adds alert helper to framework

--- lib/vesper/template/ext/helpers.rb
+++ lib/vesper/template/ext/helpers.rb
@@ -4,6 +4,14 @@ helpers do
     'active' if request.path_info.include? "/#{path}"
   end
   
+  def alert
+    unless session[:alert].nil?
+      msg = session[:alert]
+      session[:alert] = nil
+      "<div id='alert'>#{msg}</div>"
+    end
+  end
+  
   def hidden
     'display: none;'
   end
