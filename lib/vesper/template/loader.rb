7695955edfd7814093d9a0732fe771271f8d9a62
Jarrod Taylor <jarrodtaylor@me.com>
2011-12-16 14:17:13 -0500

    Revises framework loader

--- lib/vesper/template/loader.rb
+++ lib/vesper/template/loader.rb
@@ -1,15 +1,9 @@
 # Use the gems in Gemfile
-  require 'bundler'
-  Bundler.require
+require 'bundler'
+Bundler.require
 
 # Directories to load, in order
-  [
-    'config',
-    'ext',
-    'plugins/**/config',
-    'plugins/**/application',
-    'application'
-  ].each {|dir| Dir["./#{dir}/**/*.rb"].each {|file| require file}}
+['config', 'plugins/**/config', 'plugins/**/application', 'application'].each {|dir| Dir["./#{dir}/**/*.rb"].each {|file| require file}}
 
 # Tell DataMapper that all the models are ready
-  DataMapper.finalize
+DataMapper.finalize
