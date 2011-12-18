2197db76e64440c2fc3ca3b3c9993319ead42e1b
Jarrod Taylor <jarrodtaylor@me.com>
2011-12-18 01:33:59 -0500

    Removes framework loader file

--- lib/vesper/framework/loader.rb
+++
@@ -1,9 +0,0 @@
-# Use the gems in Gemfile
-require 'bundler'
-Bundler.require
-
-# Directories to load, in order
-['config', 'plugins/**/config', 'plugins/**/application', 'application'].each {|dir| Dir["./#{dir}/**/*.rb"].each {|file| require file}}
-
-# Tell DataMapper that all the models are ready
-DataMapper.finalize
