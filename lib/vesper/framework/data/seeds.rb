dc6fb39758105e1acdca348dca5c1bdd4a6c370f
Jarrod Taylor <jarrodtaylor@me.com>
2011-12-31 15:53:57 -0500

    Purges DataMapper from default framework

--- lib/vesper/framework/data/seeds.rb
+++
@@ -1,7 +0,0 @@
-# For adding required data to a new database
-# 
-# Usage:
-#  >> rake db:seed
-# 
-# Example:
-# User.create(:email => 'jarrodtaylor@me.com')
--- lib/vesper/framework/data/transmogrify.rb
+++
@@ -1,7 +0,0 @@
-# For modifying existing data
-# 
-# Usage:
-#  >> rake db:transmogrify
-# 
-# Example:
-# User.first.update(:email => 'new.email@example.com')
