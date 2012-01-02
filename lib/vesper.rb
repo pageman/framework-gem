c9b740870e28220667ec71d18b875a7ffdfb4f88
Jarrod Taylor <jarrodtaylor@me.com>
2012-01-02 11:12:42 -0500

    Adds framework error pages

--- lib/vesper.rb
+++ lib/vesper.rb
@@ -18,5 +18,13 @@ module Vesper
     'application',
     'plugins/**/hooks/post-boot.rb'
   ].each {|dir| Dir["./#{dir}/**/*.rb"].each {|file| require file}}
-
+    
+  error(400) { redirect '/400.html' }
+  error(401) { redirect '/401.html' }
+  error(403) { redirect '/403.html' }
+  error(404) { redirect '/404.html' }
+  error(408) { redirect '/408.html' }
+  error(500) { redirect '/500.html' }
+  error(502) { redirect '/502.html' }
+  
 end
