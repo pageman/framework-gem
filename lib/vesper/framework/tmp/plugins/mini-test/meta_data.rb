fa0a1e2e0878ecfb593abebf6a01a4a0c8a8f585
Jarrod Taylor <jarrodtaylor@me.com>
2012-01-13 00:01:37 -0500

    Adds mini-test as a default framework plugin

---
+++ lib/vesper/framework/tmp/plugins/mini-test/meta_data.rb
@@ -0,0 +1,7 @@
+META_DATA = {
+  name: 'mini-test',
+  version: '1.0.0',
+  authors: ['Jarrod Taylor'],
+  homepage: 'https://github.com/jarrodtaylor/vpi-mini-test',
+  source: 'git@github.com:jarrodtaylor/vpi-mini-test.git'
+}
