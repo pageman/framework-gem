dc12267f715bd74f8424f95052298310bd77b9fb
Jarrod Taylor <jarrodtaylor@me.com>
2012-01-13 14:07:48 -0500

    Removes the need for plugin meta data

--- lib/vesper/plugin/meta_data.rb
+++
@@ -1,7 +0,0 @@
-META_DATA = {
-  name: 'my-nifty-plugin', # This is what your plugin directory will be called
-  version: '0.0.1',
-  authors: ['Your name here...'],
-  homepage: 'http://example.com/yourplugin',
-  source: '' # Link to git repo for installation
-}
