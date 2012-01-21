438c446c992739e9f6531f82605d0cb912c90f68
Jarrod Taylor <jarrodtaylor@me.com>
2012-01-21 17:57:35 -0500

    Removes custom framework error pages unless in production

--- lib/vesper.rb
+++ lib/vesper.rb
@@ -26,13 +26,16 @@ module Vesper
   
     Dir["./plugins/**/post-boot.rb"].each {|file| require file}
     
-    error(400) { redirect '/400.html' }
-    error(401) { redirect '/401.html' }
-    error(403) { redirect '/403.html' }
-    error(404) { redirect '/404.html' }
-    error(408) { redirect '/408.html' }
-    error(500) { redirect '/500.html' }
-    error(502) { redirect '/502.html' }    
+    configure :production do
+      error(400) { redirect '/400.html' }
+      error(401) { redirect '/401.html' }
+      error(403) { redirect '/403.html' }
+      error(404) { redirect '/404.html' }
+      error(408) { redirect '/408.html' }
+      error(500) { redirect '/500.html' }
+      error(502) { redirect '/502.html' }
+    end
+    
   end
   
   def self.load_tasks
