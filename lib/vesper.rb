17d73aad1b13cb82b0a77a15485dad52efbc9d54
Jarrod Taylor <jarrodtaylor@me.com>
2012-01-22 12:08:52 -0500

    Updates framework error handler redirects

--- lib/vesper.rb
+++ lib/vesper.rb
@@ -27,13 +27,13 @@ module Vesper
     Dir["./plugins/**/post-boot.rb"].each {|file| require file}
     
     configure :production do
-      error(400) { redirect '/400.html' }
-      error(401) { redirect '/401.html' }
-      error(403) { redirect '/403.html' }
-      error(404) { redirect '/404.html' }
-      error(408) { redirect '/408.html' }
-      error(500) { redirect '/500.html' }
-      error(502) { redirect '/502.html' }
+      error(400) { redirect '/errors/400.html' }
+      error(401) { redirect '/errors/401.html' }
+      error(403) { redirect '/errors/403.html' }
+      error(404) { redirect '/errors/404.html' }
+      error(408) { redirect '/errors/408.html' }
+      error(500) { redirect '/errors/500.html' }
+      error(502) { redirect '/errors/502.html' }
     end
     
   end
